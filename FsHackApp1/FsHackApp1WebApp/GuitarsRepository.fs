e9a271e6902906e5a4d7e8d34f835803c4f02ce1
jsakamoto <junichi.jp@gmail.com>
2013-03-23 16:45:29 +0900

    Add variety of query methods at repository.

--- FsHackApp1/FsHackApp1WebApp/GuitarsRepository.fs
+++ FsHackApp1/FsHackApp1WebApp/GuitarsRepository.fs
@@ -5,3 +5,9 @@ type GuitarsRepository() =
         use context = new FsMvcAppEntities()
         query { for g in context.Guitars do select g }
         |> Seq.toList
+
+    member x.GetByName name =
+        use context = new FsMvcAppEntities()
+        query { for g in context.Guitars do where (g.Name = name) }
+        |> Seq.toList
+        
