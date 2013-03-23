3d9c573314ced7571a5836737bae21be56881bde
jsakamoto <junichi.jp@gmail.com>
2013-03-23 16:58:00 +0900

    More functional repository.

--- FsHackApp1/FsHackApp1WebApp/GuitarsController.fs
+++ FsHackApp1/FsHackApp1WebApp/GuitarsController.fs
@@ -1,15 +1,25 @@
 ﻿namespace FsWeb.Controllers
-
+open System
 open System.Web.Mvc
 open FsWeb.Models
 open FsWeb.Repositories
+open Repository
 
 [<HandleError>]
-type GuitarsController(repository : GuitarsRepository) =
+type GuitarsController(context:IDisposable, ?repository) =
     inherit Controller()
 
-    new() = new GuitarsController(GuitarsRepository())
+    let fromRepository = 
+        match repository with
+        |Some v -> v
+        |_-> (context :?> FsMvcAppEntities).Guitars |> Repository.get
+
+    new() = new GuitarsController(new FsMvcAppEntities())
 
     member this.Index() =
-        repository.GetAll()
+        getAll() |> fromRepository
         |> this.View
+
+    override x.Dispose disposing =
+        context.Dispose()
+        base.Dispose disposing
---
+++ FsHackApp1/FsHackApp1WebApp/Repository.fs
@@ -0,0 +1,10 @@
+﻿namespace FsWeb.Repositories
+open System
+open System.Linq
+
+module Repository =
+    let get (source:IQueryable<_>) queryFn =
+        queryFn source |> Seq.toList
+
+    let getAll () =
+        fun s -> query { for x in s do select x }
